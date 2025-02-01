55850c23c05d1a5de74c2c311d5f66d85bb0f35f
Chetan-Satone <chetan.k.rock@gmail.com>
2025-02-01 22:14:51 +0530

    solved

---
+++ c  program/perimeterofrectangle.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+    int a;
+    int b;
+    printf("enter side a length: ");
+    scanf("%d", &a);
+
+    printf("Enter side b breath: ");
+    scanf("%d",&b);
+
+
+    printf("Area of rectangle: %d ", a * b * 2);
+
+
+
+
+    return 0;
+}
