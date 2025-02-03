725af3619a71359e7fa12256719be69eac33721a
Chetan-Satone <chetan.k.rock@gmail.com>
2025-02-03 10:20:57 +0530

    solved

---
+++ c  program/pratice2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+// int main(){
+//      // odd -> 0
+//      // even -> 1
+
+//     int x;
+//     printf(" enter a number: ");
+//     scanf("%d",&x);
+//     printf("%d", x % 2 == 0);
+// }
+
+int main (){
+
+    int itsmonday = 0;
+    int itsraining = 0;
+
+    printf("%d\n", itsmonday || itsraining);
+    return 0;
+}
